9edd432feafceaed58fa81d9dad36daad9567ebd
Ba4iks <hearthtonehero@gmail.com>
2022-08-08 13:35:59 +0200

    JavaCore

--- HW3/HW3.3/untitled/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Person pers1 = new Person();
-        pers1.input();
-        pers1.output();
-        pers1.changeName("Max","Bosk");
-        pers1.output();
-    }
-}
--- Homework OOP2/src/Payment.java
+++
@@ -1,3 +0,0 @@
-public interface Payment {
-    public  int calculatePay();
-}
