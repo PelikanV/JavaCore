6d198420c92f7a1a2903d5e17be984bd4f30f1e1
Ivan <109279068+PelikanV@users.noreply.github.com>
2022-08-08 14:30:22 +0300

    Delete Homework OOP2 directory

--- Homework OOP2/src/SortBySalary.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class SortBySalary implements  Comparator<Employee>{
-    public int compare(Employee a, Employee b)
-    {
-
-        return a.calculatePay() - b.calculatePay();
-    }
-}
